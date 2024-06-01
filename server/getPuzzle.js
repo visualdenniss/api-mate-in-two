29fce1b27e4d6cb43ec6b58863d11a4477cec1e6
visualdenniss <visualdennis@gmail.com>
2024-06-01 15:10:03 +0200

    db connection update, nodemon, get random single puzzle via query, instead of all and then selecting one randomly from the list in getPuzzle.js

--- server/getPuzzle.js
+++ server/getPuzzle.js
@@ -1,60 +1,50 @@
-const {connectToDb, getDb} = require('./db')
+const { connectToDb, getDb } = require("./db");
+const { Board } = require("./toFen");
 
-const {
-    parseSquare,
-    __setv,
-    Piece,
-    ParsePiece,
-    FairyHelper,
-    Board,
-    xfen2spritedecl,
-    __fairyHelper,
-    board
-} = require('./toFen')
-
-
-const getPuzzle = async (req,res) => {
-    try {
-
-        connectToDb((err)=>{
-            if(!err){
-                db = getDb()
-            }
-
-            let puzzles = []
-            db.collection('problems')
-            .find()
-            .forEach(puzzle => puzzles.push(puzzle))
-            .then(()=>{
-                let random = Math.floor(Math.random()*1000)
-
-                const puzzle = puzzles[random]
-                const puzzleAuthor = puzzle.authors
-                const puzzleId = puzzle.id
-                const puzzleSource = puzzle.source 
-
-                var board = new Board();
-                const algebraic = puzzle.algebraic
-                board.fromAlgebraic(algebraic);
-                const fen = board.toFen().replace(/s/g, 'n').replace(/S/g, 'N');
-                
-
-                const matePuzzle = {
-                    puzzleAuthor,
-                    puzzleId,
-                    puzzleSource,
-                    fen
-                }
-
-                res.status(200).json(matePuzzle)
-            })
-        })
-
-    } catch (error) {
-        console.log(error);
+const getPuzzle = (req, res) => {
+  connectToDb((err) => {
+    if (err) {
+      console.error("Failed to connect to database:", err);
+      return res.status(500).json({ error: "Database connection failed" });
     }
-}
 
-module.exports = {
-    getPuzzle
-}
+    const db = getDb();
+    const puzzlesCollection = db.collection("problems");
+
+    puzzlesCollection
+      .aggregate([{ $sample: { size: 1 } }])
+      .toArray()
+      .then((randomPuzzle) => {
+        if (randomPuzzle.length === 0) {
+          return res.status(404).json({ error: "No puzzles found" });
+        }
+
+        const puzzle = randomPuzzle[0];
+        const puzzleAuthor = puzzle.authors;
+        const puzzleId = puzzle.id;
+        const puzzleDbID = puzzle._id; // use _id since it's a MongoDB ObjectId
+        const puzzleSource = puzzle.source;
+
+        var board = new Board();
+        const algebraic = puzzle.algebraic;
+        board.fromAlgebraic(algebraic);
+        const fen = board.toFen().replace(/s/g, "n").replace(/S/g, "N");
+
+        const matePuzzle = {
+          puzzleAuthor,
+          puzzleId,
+          puzzleSource,
+          fen,
+          puzzleDbID,
+        };
+
+        res.status(200).json(matePuzzle);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+      });
+  });
+};
+
+module.exports = { getPuzzle };
