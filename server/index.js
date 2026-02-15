8549382b5f376d4f5608ceb73e1ab8943558e5a0
visualdennis <deniztasdelen@denizair.fritz.box>
2026-02-15 12:25:27 +0100

    removed fenToImg from backend logic

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-require("dotenv").config();
-const bodyParser = require("body-parser"); // Import bodyParser for parsing POST request bodies
-const { getPuzzles } = require("./getPuzzles");
-const { getPuzzle } = require("./getPuzzle");
-const fenToImg = require("./fenToImg");
+const cors = require('cors');
+require('dotenv').config();
+const bodyParser = require('body-parser'); // Import bodyParser for parsing POST request bodies
+const { getPuzzles } = require('./getPuzzles');
+const { getPuzzle } = require('./getPuzzle');
 // const { getAllAuthorList } = require("./getAllAuthorList");
 
 // Middleware for parsing JSON bodies
@@ -15,12 +14,9 @@ app.use(express.json());
 
 // routes
 
-app.get("/", getPuzzles);
-app.post("/fen", fenToImg); // Change route to accept POST requests
-// app.get("/api/authors", getAllAuthorList); // Gets all authors
+app.get('/', getPuzzles);
+app.get('/:id', getPuzzle);
 
-app.get("/:id", getPuzzle);
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
